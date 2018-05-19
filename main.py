238013cec363e969f3df7cb985d3f4acf6c1a183
Franko1307 <franciscogonzalez1307@gmail.com>
2018-05-18 23:24:45 -0700

    añadiendo funcionalidades

--- main.py
+++ main.py
@@ -3,113 +3,23 @@ import random
 import logging
 import os
 import spacy
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+from operator import itemgetter
+from operator import add
 import es_core_news_sm
+from functools import reduce
 
-os.environ['NLTK_DATA'] = os.getcwd() + '/nltk_data'
-
-from config import FILTER_WORDS
 
 logging.basicConfig()
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-GREETING_KEYWORDS = ("hola", "hey", "saludos", "que pedo?", "que tranza",)
 
-GREETING_RESPONSES = ["que pex", "hey", "que pasa", "que pedo we"]
-
-
-
-def check_for_greeting(sentence):
-    for word in sentence:
-        if word.text.lower() in GREETING_KEYWORDS:
-            return random.choice(GREETING_RESPONSES)
-
-
-NONE_RESPONSES = [
-    "NANI????",
-    "NANDATO???",
-    "Demasiado para mi we",
-    "La neta, no me importa xD",
-    "No mames",
-    "Esa wea ke?",
-    "No entendi we",
-]
-# end
-
-COMMENTS_ABOUT_SELF = [
-    "Yo me amo mucho",
-    "He sido programado por los mejores",
-    "La vida es hermosa, porque yo existo",
-]
-# end
-
-
-class UnacceptableUtteranceException(Exception):
-    pass
-
-def broback(sentence):
+def broback(sentence, orden):
     logger.info("Broback: respond to %s", sentence)
-    resp = respond(sentence)
-    return resp
-
-def construct_response(pronoun,aux, noun, verb, det):
-    resp = []
-
-    if aux:
-
-        aux_word = aux.text
-        print("aux word: ", aux_word)
-        if aux_word == 'soy':  
-            resp.append(random.choice(["nah, no eres", "tal ves seas"]))
-    if det is not "":
-        resp.append(det.text)
-    if noun is not "":
-        resp.append(noun.text)
-    
-    resp.append(random.choice(("we", "")))
-
-    return " ".join(resp)
-# end
-
-
-def check_for_comment_about_bot(pronoun, noun, adjective):
-    resp = None
-    if pronoun is not "":
-        if pronoun.text =='tu' or pronoun.text == "tu" and (noun or adjective):
-            if noun is not "":
-
-                if random.choice((True, False)):
-                    resp = random.choice(SELF_VERBS_WITH_NOUN_CAPS_PLURAL).format(**{'noun': sing_to_plural(noun)})
-                else:
-                    resp = random.choice(SELF_VERBS_WITH_NOUN_LOWER).format(**{'noun': noun})
-            else:
-                resp = random.choice(SELF_VERBS_WITH_ADJECTIVE).format(**{'adjective': adjective})
-    return resp
-
-def sing_to_plural(noun):
-    var = noun.text.split(',')
-    if var[-1] not in ['a', 'e', 'i', 'o', 'u']: 
-        var.append('es')
-    else:
-        var.append('s')
-    return ''.join(var) 
-
-SELF_VERBS_WITH_NOUN_CAPS_PLURAL = [
-    "la neta estoy bien pesado en {noun}",
-]
-
-SELF_VERBS_WITH_NOUN_LOWER = [
-    "si, pero se mucho de {noun}",
-    "Mis compas siempre me preguntan de {noun}",
-]
-
-SELF_VERBS_WITH_ADJECTIVE = [
-    "a quien le dices {adjective} we", 
-    "tal vez sea {adjective} we",
-    "soy tan {adjective} como tu we",
-    "ya lo se, me paso de lanza",
-]
-# end
+    orden = respond(sentence,orden)
+    return orden
 
 def preprocess_text(sentence):
     cleaned = []
@@ -123,75 +33,154 @@ def preprocess_text(sentence):
 
     return ' '.join(cleaned)
 
-def respond(sentence):
-    cleaned = preprocess_text(sentence)
+import json
+def open_json(route):
+    with open("data/json/"+route+".json") as f:
+        data = json.load(f)
+    return data
 
+#Funcion encargada de agarrar la orden y transformarla en una orden mas general, es decir cosas como "pedir, solicitar, dar" -> "ordenar"
+def process_order(orden):
+    json_ordenes = open_json("ordenes")
+    for key, value in json_ordenes.items():
+        if orden in value:
+            return key
+    return orden
+
+#funcion que regresa posibles ordenes que el usuario pudiera dar
+def get_orders():
+    return reduce(add, open_json("ordenes").values())
+
+#Funcion que filtra los verbos en una sentencia y los retorna
+def get_verbs(sentence):
+    verbs = []
+    for word in sentence:
+        #print(word, word.tag_)
+        if word.pos_ == 'VERB':
+            verbs.append(word.text)
+    return verbs
+
+#Funcion que nos permite deducir que es lo que el usuario quiere que hagamos, ejemplos: ordenar, remover, cambiar, etc...
+def detect_order(sentence):
+
+    verbs = get_verbs(sentence)
+
+    possible_orders = get_orders()
+
+    candidates = []
+
+    for verb in verbs:
+        candidates.append(process.extractOne(verb,possible_orders))
+    if len(candidates) > 0:
+        orden, score = max(candidates, key=itemgetter(1))
+    else:
+        #ordenar es por default.
+        orden = "ordenar"
+        score = 0
+    print("Orden: ", orden, " candidatos: ", candidates)
+    if score > 75:
+        return process_order(orden)
+    return "ordenar"
+
+#Funcion encargada de detectar y unir todos los nouns
+def get_noun(sentence):
+    nouns = []
+
+    idx = 0
+    isClosed = True
+    noun = ""
+    for word in sentence:
+        if isClosed:
+            noun = ""
+            isClosed = False
+        if word.pos_ == "NOUN" or word.pos_ == "ADP":
+            noun += word.text + " "
+        else:
+            isClosed = True
+        if isClosed and noun != "":
+            nouns.append((noun,1))
+
+    if noun != "":
+        nouns.append((noun,1))
+
+    return nouns
+#Esta funcion busca en la oracion, los adjetivos que son los elementos que el usuario ordeno y los aniade a la lista de compras
+def add_to_list(sentence, order):
+    #Obtenemos los nouns, cosas como pizza de queso, hamburguesa, coca de dieta, etc... (Funcionando 60%)
+    nouns = get_noun(sentence)
+    response = ""
+    #TODO: La variable qt representa la cantidad, esta por default a 1, pero se deberia poder leer esa cantidad en la sentencia dada por el usuario y pedirla en caso de que no se nos solicite, por ejemplo en el caso de : Me da unas pizzas. Debemos pedirle al usuario que reingrese su sentencia con la informacion mas completa
+    for noun in nouns:
+        #Estructura de noun : (NOUN, CANTIDAD)
+        qt = noun[1]
+        if noun[0] in order:
+            response = "Claro, le he agregado " + noun + " ha su pedido."
+            order[noun[0]] += qt
+        else:
+            order[noun[0]] = qt
+    return response,order
+
+def remove_from_list(sentence, order):
+    nouns = get_noun(sentence)
+    response = ""
+    for noun in nouns:
+        qt = noun[1]
+        if noun[0] in order:
+            response = "Claro, le he removido " + noun[0] + " de su pedido"
+            order[noun[0]] -= qt
+        else:
+            response = "No recuerdo que me haya pedido " + noun[0] + " por favor, pruebe de nuevo."
+    return response,order
+
+
+#Funcion encargada de responder al usuario
+def respond(sentence, list):
+    #Se carga lo relacionado a spacy
     nlp = es_core_news_sm.load()
     parsed = nlp(sentence)
 
-    pronoun, aux, noun, adjective, verb, det = find_candidate_parts_of_speech(parsed)
-
-    resp = check_for_comment_about_bot(pronoun, noun, adjective)
-
-    if not resp:
-        resp = check_for_greeting(parsed)
-
-    if not resp:
-        if not pronoun:
-            resp = random.choice(NONE_RESPONSES)
-        elif pronoun.text == 'tu' and not verb:
-            resp = random.choice(COMMENTS_ABOUT_SELF)
-        else:
-            resp = construct_response(pronoun,aux, noun, verb,det)
-
-    if not resp:
-        resp = random.choice(NONE_RESPONSES)
-
-    logger.info("Returning phrase '%s'", resp)
-
-    filter_response(resp)
-
-    return resp
-
-def find_candidate_parts_of_speech(parsed):
-
-    pronoun, aux, noun, adjective, verb, det =[], [], [], [], [], []
-
+    #Para imprimir los tags que spacy nos ofrece
     for word in parsed:
-        print(word.pos_,word.tag_)
-        if word.pos_ == 'PRON':
-            pronoun.append(word)
-        elif word.pos_ is "AUX":
-            aux.append(word)    
-        elif word.pos_ == 'NOUN':
-            noun.append(word)
-        elif word.pos_ == 'ADJ':
-            adjective.append(word)
-        elif word.pos_ == 'VERB':
-            verb.append(word)
-        elif word.pos_ == 'DET':
-            det.append(word)
+        print(word.text, word.pos_, word.tag_)
 
-    pronoun.append('')
-    aux.append('')
-    noun.append('')
-    adjective.append('')
-    verb.append('')
-    det.append('')
+    #Detectamos que orden desea el cliente (Funcionando 80%, pasa los testeos pero no la he testeado bien)
+    orden = detect_order(parsed)
 
-    print(pronoun, aux, noun, adjective, verb, det)
-    logger.info("Pronoun=%s, noun=%s, adjective=%s, verb=%s", pronoun[0], noun[0], adjective[0], verb[0])
-    return pronoun[0],aux[0], noun[0], adjective[0], verb[0], det[0]
+    #Dependiendo de la orden hacemos lo que se nos pida
+    #Nota: Ordenar es la mas completa, remover esta mas o menos completa, cambiar esta incompleta y se espera que podamos brindar mas ordenes
+    if orden == "ordenar":
+        list = add_to_list(parsed,list)
+    elif orden == "remover":
+        remove_from_list(parsed, list)
+    elif orden == "cambiar":
+        change_from_list(parsed, list)
+    else:
+        return orden
 
-def filter_response(resp):
-    """Don't allow any words to match our filter list"""
-    tokenized = resp.split(' ')
-    for word in tokenized:
-        if '@' in word or '#' in word or '!' in word:
-            raise UnacceptableUtteranceException()
-        for s in FILTER_WORDS:
-            if word.lower().startswith(s):
-                raise UnacceptableUtteranceException()
-    
+    return orden
+
+#Testeo
+import unittest
+
+#Clase para testeo de casos
+class MyTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(broback("Quisiera ordenar una pizza",{}), "ordenar")
+        self.assertEqual(broback("Quisiera una pizza",{}), "ordenar")
+        self.assertEqual(broback("Me das una pizza de queso",{}), "ordenar")
+        self.assertEqual(broback("Me quitas la pizza de anchoas por favor",{}), "remover")
+        self.assertEqual(broback("Va a ser una hamburguesa con papas",{}), "ordenar")
+        self.assertEqual(broback("Me remueves las salchichas por favor",{}), "remover")
+        self.assertEqual(broback("Quiero ordenar tres pizzas por favor",{}), "ordenar")
+        self.assertEqual(broback("Quiero ordenar unas pizzas  por favor",{}), "ordenar")
+        self.assertEqual(broback("Quiero unas 3 pizzas por favor",{}), "ordenar")
+        self.assertEqual(broback("Me da unas cuatro hamburguesas",{}), "ordenar")
+        logger.info("OKAY, EVERYTHING LOOKS GOOD!")
+
+
 if __name__ == '__main__':
-	print(broback(sentence))
+    tester = MyTest()
+    tester.test()
+
+
+
