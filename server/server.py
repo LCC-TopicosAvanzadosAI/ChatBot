4fb7583cfdcbd87c192d4c444d90e1b93d1ada8a
Franko1307 <franciscogonzalez1307@gmail.com>
2018-05-20 13:27:28 -0700

    data cambios

--- server/server.py
+++ server/server.py
@@ -10,16 +10,19 @@ app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def static_page():
-	    return render_template('index.html')
+    return render_template('index.html')
 
 data = {}
 @app.route('/lcc-bot', methods=['POST'])
 def login():
-        if request.method == 'POST':
-                datafromjs = request.form['mydata']
+    global data
+    response = ""
+    if request.method == 'POST':
+        datafromjs = request.form['mydata']
         
         response, data = broback(datafromjs, data)
-        return response
+
+    return response
 
 if __name__ == "__main__":
     app.run()
